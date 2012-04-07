d590b86abcfe816a3cec672652fe07677e97d3e6
Pratap Chakravarthy <prataprc@gmail.com>
2012-04-07 17:47:02 +0530

    HTTP server.

--- pluggdapps/server.py
+++ pluggdapps/server.py
@@ -4,8 +4,7 @@
 
 # -*- coding: utf-8 -*-
 
-import socket
-from   BaseHTTPServer
+import time, socket
 
 from   pluggdapps   import __version__
 from   plugincore   import Plugin, implements
@@ -36,6 +35,15 @@ _default_settings['maxprocess']     = {
     'types'   : (int,),
     'help'    : "The maximum number of worker process to handle incoming  "
                 "request. Default is (multiprocessing.cpu_count() * 4)."
+}
+_default_settings['maxtaskperchild']     = {
+    'default' : 1000
+    'types'   : (int,),
+    'help'    : "Number of tasks a worker process can complete before it will "
+                "exit and be replaced with a fresh worker process, to enable "
+                "unused resources to be freed. If 0, worker processes will "
+                "live as long as the pool. This parameter is directly passed "
+                "on to multiprocessing.Pool API."
 }
 _default_settings['server_name']    = {
     'default' : socket.gethostname(),
@@ -86,7 +94,9 @@ _default_settings['nodelay']                    = {
                 "socket option."
 }
 
-class HTTPServer( object ):
+class HTTPServer( Plugin ):
+
+    implements( IHTTPServer )
 
     _interrupt  = False
 
@@ -96,8 +106,11 @@ class HTTPServer( object ):
     _software   = "'pluggdapps / %s Server'" % __version__
     "SERVER_SOFTWARE entry in the WSGI environ."
 
-    _ready = False
+    _tickloop   = False
     "Internal flag which marks whether the socket is accepting connections."
+
+    _start_time = None
+    "Internal stats on when the server object started accepting connections."
 
     # TODO : To be moved as plugins.
     connection_class = HTTPConnection
@@ -112,14 +125,106 @@ class HTTPServer( object ):
     You must have the corresponding SSL driver library installed."""
 
     def __init__( self, gateway ):
+        Plugin.__init__( self )
+
+        self.bind_addr = (self['host'], self['port'])
         self.gateway = gateway
+        self.pool = multiprocessing.Pool( 
+            self['minprocess'], None, [], self['maxtaskperchild'] )
+        self._clear_stats()
 
-        self.requests = ThreadPool( self, minthreads, maxthreads )
+    def _clear_stats( self ):
+        self._start_time = None
+        self._run_time = 0
+        self.stats = {
+            'Enabled'     : False,
+            'Bind Address': lambda s: repr( self.bind_addr ),
+            'Run time'    : lambda s: (not s['Enabled']) and -1 or self.runtime(),
+            'Accepts': 0,
+            'Accepts/sec': lambda s: s['Accepts'] / self.runtime(),
+            'Queue': lambda s: getattr(self.requests, "qsize", None),
+            'Threads': lambda s: len(getattr(self.requests, "_threads", [])),
+            'Threads Idle': lambda s: getattr(self.requests, "idle", None),
+            'Socket Errors': 0,
+            'Requests': lambda s: (not s['Enabled']) and -1 or sum([w['Requests'](w) for w
+                                       in s['Worker Threads'].values()], 0),
+            'Bytes Read': lambda s: (not s['Enabled']) and -1 or sum([w['Bytes Read'](w) for w
+                                         in s['Worker Threads'].values()], 0),
+            'Bytes Written': lambda s: (not s['Enabled']) and -1 or sum([w['Bytes Written'](w) for w
+                                            in s['Worker Threads'].values()], 0),
+            'Work Time': lambda s: (not s['Enabled']) and -1 or sum([w['Work Time'](w) for w
+                                         in s['Worker Threads'].values()], 0),
+            'Read Throughput': lambda s: (not s['Enabled']) and -1 or sum(
+                [w['Bytes Read'](w) / (w['Work Time'](w) or 1e-6)
+                 for w in s['Worker Threads'].values()], 0),
+            'Write Throughput': lambda s: (not s['Enabled']) and -1 or sum(
+                [w['Bytes Written'](w) / (w['Work Time'](w) or 1e-6)
+                 for w in s['Worker Threads'].values()], 0),
+            'Worker Threads': {},
+            }
+        logging.statistics["CherryPy HTTPServer %d" % id(self)] = self.stats
 
-        if not server_name:
-            server_name = socket.gethostname()
-        self.server_name = server_name
-        self.clear_stats()
+    def start( self ):
+        self._interrupt = None
+
+        # Support both AF_INET and AF_INET6 family of address
+        host, port = self.bind_addr
+        try:
+            info = socket.getaddrinfo( 
+                        host, port, socket.AF_UNSPEC, 
+                        socket.SOCK_STREAM, 0, socket.AI_PASSIVE 
+                   )
+        except socket.gaierror:
+            if ':' in self.bind_addr[0]:
+                info = [( socket.AF_INET6, socket.SOCK_STREAM,
+                           0, "", self.bind_addr + (0, 0) )]
+            else:
+                info = [( socket.AF_INET, socket.SOCK_STREAM,
+                          0, "", self.bind_addr )]
+
+        msg = "No socket could be created"
+        self.socket = None
+        for af, socktype, proto, caname, sa in info:
+            try:
+                self.bind( af, socktype, proto )
+                break
+            except socket.error:
+                self.socket.close() if self.socket else None
+                self.socket = None
+
+        if not self.socket:
+            raise socket.error( msg ).
+
+        # Timeout so KeyboardInterrupt can be caught on Win32
+        self.socket.settimeout(1)
+        # Listen
+        self.socket.listen( self['request_queue_size'] )
+
+        self._tickloop = True
+        while self._tickloop :
+            self._start_time = time.time()
+            try : 
+                self.tick()
+            except (KeyboardInterrupt, SystemExit) :
+                raise
+            except :
+                # TODO : Log error message
+
+            if self.interrupt:
+                # Wait for self.stop() to complete. See _set_interrupt.
+                while self.interrupt is True : time.sleep(0.1)
+                if self.interrupt : raise self.interrupt
+
+    # ISettings interface methods.
+    def normalize_settings( self, settings ):
+        return settings
+
+    def default_settings():
+        return _default_settings.items()
+
+    def web_admin( self, settings ):
+        """Web admin interface is not allowed for this plugin."""
+        pass
 
 
 class RequestHandler( BaseHTTPServer.BaseHTTPRequestHandler ):
@@ -165,21 +270,3 @@ class RequestHandler( BaseHTTPServer.BaseHTTPRequestHandler ):
         pass
 
 
-class NativeServer( Plugin ):
-    implements( IHTTPServer )
-
-    def serve( self, appsettings ):
-        server_address = ( self['host'], self['port'] )
-        server = HTTPServer( server_address, RequestHanlder )
-        server.serve_forever()
-
-    # ISettings interface methods.
-    def normalize_settings( self, settings ):
-        return settings
-
-    def default_settings():
-        return _default_settings.items()
-
-    def web_admin( self, settings ):
-        """Web admin interface is not allowed for this plugin."""
-        pass
