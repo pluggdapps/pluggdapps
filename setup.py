3df5565c00e9f3f8eacdc09b310b3e4c2cdda4b7
Pratap Chakravarthy <prataprc@gmail.com>
2013-06-12 14:51:45 +0530

    Preparing for release 0.42dev

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         ]
     },
     install_requires=[                      # setuptools
-        'tayra>=0.42dev',
+        'tayra>=0.43dev',
         'tayrakit>=0.2dev',
     ],
     extras_require={},                      # setuptools
